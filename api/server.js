41ad4c4f556d95cba2c162de2b3ca90c2ac0680e
brennanlawrence <brennanlawrence2002@gmail.com>
2021-01-15 11:19:05 -0700

    Trying again

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ server.use("/api/actions", actionsRoute);
 server.use("/api/projects", projectsRoute);
 
 server.get("/", (req, res) => {
-  res.status(200).json({ message: "Server is up." });
+  res.json({ message: "Server is up." });
 });
 
 // Complete your server here!
